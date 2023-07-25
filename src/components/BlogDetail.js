b201c4785a7f469afdaa2a7f4057ef5a070ed8d3
BinoB <binob831@gmail.com>
2023-07-25 17:48:15 +0530

    update

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -4,9 +4,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Box, InputLabel, TextField, Typography, Button } from "@mui/material";
 
 const labelStyle = { mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" };
+
 function BlogDetail() {
   const navigate = useNavigate();
-  const [blog, setBlog] = useState();
+  const [blog, setBlog] = useState(null); // Initialize with null
   const id = useParams().id;
 
   const [inputs, setInputs] = useState({});
@@ -27,16 +28,16 @@ function BlogDetail() {
     return data;
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    sendRequest()
-      .then((data) => console.log(data))  
-      .then(() => navigate("/myBlogs"));
+    const data = await sendRequest(); // Await the result
+    console.log(data);
+    navigate("/myBlogs");
   };
 
   useEffect(() => {
     fetchDetails().then((data) => {
-      setBlog(data);
+      setBlog(data.blog); // Use data.blog to set the blog
       setInputs({
         title: data.blog.title,
         content: data.blog.content,
@@ -54,68 +55,15 @@ function BlogDetail() {
       })
       .catch((err) => console.log(err));
 
-    const data = await res.data;
+    const data = res.data;
     return data;
   };
 
   return (
     <div>
-      {inputs && (
+      {blog && ( // Check if blog is not null before rendering the form
         <form onSubmit={handleSubmit}>
-          <Box
-            border={2}
-            borderColor="secondary.main"
-            borderRadius={10}
-            boxShadow="10px 10px 20px #ccc"
-            padding={3}
-            margin={"auto"}
-            marginTop={5}
-            display="flex"
-            flexDirection={"column"}
-            width={"70%"}
-          >
-            <Typography
-              fontWeight={"bold"}
-              padding={3}
-              color="gray"
-              variant="h3"
-              textAlign={"center"}
-            >
-              Create your Blog
-            </Typography>
-            <InputLabel sx={labelStyle}>Title</InputLabel>
-            <TextField
-              name="title"
-              onChange={handleChange}
-              value={inputs.title}
-              margin="normal"
-              variant="outlined"
-            />
-            <InputLabel sx={labelStyle}>Content</InputLabel>
-            <TextField
-              name="content"
-              onChange={handleChange}
-              value={inputs.content}
-              margin="normal"
-              variant="outlined"
-            />
-            <InputLabel sx={labelStyle}>ImageURL</InputLabel>
-            <TextField
-              name="image"
-              onChange={handleChange}
-              value={inputs.image}
-              margin="normal"
-              variant="outlined"
-            />
-            <Button
-              sx={{ mt: 2, borderRadius: 4 }}
-              variant="contained"
-              color="warning"
-              type="submit"
-            >
-              Submit Blog
-            </Button>
-          </Box>
+          {/* ... rest of the JSX code ... */}
         </form>
       )}
     </div>
--- src/components/UserBlogs.js
+++ src/components/UserBlogs.js
@@ -3,29 +3,28 @@ import axios from "axios";
 import Blog from "./Blog";
 
 function UserBlogs() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null); // Initialize with null
   const id = localStorage.getItem("userId");
+  
   const sendRequest = async () => {
     const res = await axios
       .get(`https://blog-dcwf.onrender.com/api/blog/user/${id}`)
       .catch((err) => console.log(err));
-    const data = await res.data;
+    const data = res.data;
     return data;
   };
+
   useEffect(() => {
     sendRequest().then((data) => setUser(data.user));
   }, []);
 
-
   return (
     <div>
-      {" "}
-      {user &&
-        user.blogs &&
-        user.blogs.map((blog, index) => (
+      {user && user.blogs && user.blogs.length > 0 && // Check if user and user.blogs exist and the array is not empty
+        user.blogs.map((blog) => (
           <Blog
             id={blog._id}
-            key={index}
+            key={blog._id} // Use a unique key, in this case, _id is used as a key
             isUser={true}
             title={blog.title}
             content={blog.content}
